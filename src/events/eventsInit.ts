29060d918a0fbab298731f583e6d20e972f0f6bf
CharleyRog <k.ichko.ua@gmail.com>
2023-03-09 23:44:13 +0200

    TS refactoring

--- src/events/eventsInit.ts
+++ src/events/eventsInit.ts
@@ -1,28 +1,28 @@
 // IMPORT MODULES
 
-const fs = require('fs')
-const path = require('path')
-const client = require('../client.ts')
-const { DEV_MODE } = require('../utils/devModeChecker.ts')
+import { DEV_MODE } from '../utils/devModeChecker.js'
+import client from '../client.js'
+import path from 'path'
+import fs from 'fs'
 
 // CODE
 
 const devModeEvents = ['ready']
 
-const eventFileInit = (folder, file) => {
+const eventFileInit = (folder: any, file: any): void => {
   try {
     const event = require(path.join(__dirname, `./${folder.name}`, file))
     const eventName = file.split('.')[0]
-    let pos = client
+    let pos: any = client
     console.log(`Инициализация ${folder.name} события: ${eventName}`)
     if (folder.name === 'process') pos = process
-    pos.on(eventName, (...args) => event(...args))
+    pos.on(eventName, (...args: any) => event(...args))
   } catch (error) {
     console.error(error)
   }
 }
 
-const eventsInit = () => {
+const eventsInit = (): void => {
   fs.readdirSync('./events', { withFileTypes: true })
     .filter((dirent) => dirent.isDirectory())
     .forEach((folder) => {

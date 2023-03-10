eec8709de1f4f0518d75303ab6f1cae97f4e2d16
CharleyRog <k.ichko.ua@gmail.com>
2023-03-10 20:19:56 +0200

    TS events init fix

--- src/events/eventsInit.ts
+++ src/events/eventsInit.ts
@@ -4,39 +4,42 @@ import { DEV_MODE } from '../utils/devModeChecker.js'
 import client from '../client.js'
 import path from 'path'
 import fs from 'fs'
+import config from '../config/config.json' assert { type: 'json' }
 
 // CODE
 
 const devModeEvents = ['ready']
 
-const eventFileInit = (folder: any, file: any): void => {
-  try {
-    const event = require(path.join(__dirname, `./build/${folder.name}`, file))
-    const eventName = file.split('.')[0]
-    let pos: any = client
-    console.log(`Инициализация ${folder.name} события: ${eventName}`)
-    if (folder.name === 'process') pos = process
-    pos.on(eventName, (...args: any) => event(...args))
-  } catch (error) {
-    console.error(error)
-  }
-}
+const eventFileInit = async (eventName: any, folder: any, file: any, __dirname: string): Promise<void> => {
+  const buildModeHandler = config.BUILD_MODE ? '' : '/src'
+  const eventModule = await import(path.join(__dirname, `.${buildModeHandler}}/events/${folder.name}`, file))
+  const event = eventModule.default
 
-const eventsInit = (): void => {
-  fs.readdirSync('./build/events', { withFileTypes: true })
+  let pos: any = client
+  console.log(`Инициализация ${folder.name} события: ${eventName}`)
+  if (folder.name === 'process') {
+    pos = process
+  }
+  pos.on(eventName, (...args: any) => event(...args))
+}
+const eventsInit = async (): Promise<void> => {
+  const __dirname: string = path.resolve()
+  const buildModeHandler = config.BUILD_MODE ? '' : '/src'
+  fs.readdirSync(path.join(__dirname, `.${buildModeHandler}/events`), { withFileTypes: true })
     .filter((dirent) => dirent.isDirectory())
     .forEach((folder) => {
-      fs.readdirSync(`./build/events/${folder.name}`)
-        .filter((file) => file.endsWith('.js'))
-        .forEach((file) => {
+      fs.readdirSync(path.join(__dirname, `.${buildModeHandler}/events/${folder.name}`))
+        .filter((file) => file.endsWith('.ts'))
+        .forEach(async (file) => {
+          const eventName: string = file.split('.')[0]
           if (DEV_MODE) {
-            devModeEvents.forEach((devEvent) => {
-              if (devEvent === file.split('.')[0]) {
-                eventFileInit(folder, file)
+            for (const devEvent of devModeEvents) {
+              if (devEvent === eventName) {
+                await eventFileInit(eventName, folder, file, __dirname)
               }
-            })
+            }
           } else {
-            eventFileInit(folder, file)
+            await eventFileInit(eventName, folder, file, __dirname)
           }
         })
     })

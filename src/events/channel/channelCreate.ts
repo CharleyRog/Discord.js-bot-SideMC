6e17d37d501728122031eba0543999e7dfd2f7da
CharleyRog <k.ichko.ua@gmail.com>
2023-03-11 17:48:16 +0200

    Channel logs added

--- src/events/channel/channelCreate.ts
+++ src/events/channel/channelCreate.ts
@@ -1,7 +1,32 @@
 // IMPORT MODULES
 
+import typesOfAuditLogs from '../../static/typesOfAuditLogs.js'
+import { EmbedBuilder } from '@discordjs/builders'
+import embedBuilderFoo from '../../utils/embedBuilderFoo.js'
+import client from '../../client.js'
+import isTextChannel from '../../utils/isTextChannel.js'
+import config from '../../config/config.json' assert { type: 'json' }
+
 // CODE
 
 export default async (channel: any): Promise<void> => {
-  console.log(`channelCreate: ${channel}`)
+  const auditLog = await channel.guild
+    .fetchAuditLogs({ type: typesOfAuditLogs.CHANNEL_CREATE })
+    .then((audit: { entries: { first: () => any } }) => audit.entries.first())
+
+  const embed: EmbedBuilder = embedBuilderFoo({
+    title: 'Канал создан',
+    color: '#00FF00',
+  })
+
+  embed.addFields([
+    { name: 'Название:', value: `${channel.name}`, inline: true },
+    { name: 'ID:', value: `<#${channel.id}>`, inline: true },
+    { name: 'Пользователь:', value: `<@${auditLog.executor.id}>`, inline: true },
+  ])
+
+  const logChannel = client.channels.cache.get(config.CHANNELS_ID.CHANNELS_LOGS_CHANNEL_ID)
+  if (isTextChannel(logChannel)) {
+    await logChannel.send({ embeds: [embed] })
+  }
 }
--- src/events/channel/channelUpdate.ts
+++ src/events/channel/channelUpdate.ts
@@ -1,7 +1,39 @@
 // IMPORT MODULES
 
+import typesOfAuditLogs from '../../static/typesOfAuditLogs.js'
+import { EmbedBuilder } from '@discordjs/builders'
+import embedBuilderFoo from '../../utils/embedBuilderFoo.js'
+import client from '../../client.js'
+import isTextChannel from '../../utils/isTextChannel.js'
+import config from '../../config/config.json' assert { type: 'json' }
+
 // CODE
 
 export default async (oldChannel: any, newChannel: any): Promise<void> => {
-  console.log(`channelUpdate: ${oldChannel} | ${newChannel}`)
+  const auditLog = await newChannel.guild
+    .fetchAuditLogs({ type: typesOfAuditLogs.CHANNEL_UPDATE })
+    .then((audit: { entries: { first: () => any } }) => audit.entries.first())
+
+  const embed: EmbedBuilder = embedBuilderFoo({
+    title: 'Канал обновлен',
+    color: '#FFFF00',
+  })
+
+  if (newChannel.name !== oldChannel.name) {
+    embed.addFields([
+      { name: 'Старое название:', value: oldChannel.name, inline: true },
+      { name: 'Новое название:', value: newChannel.name, inline: true },
+    ])
+  }
+
+  embed.addFields([
+    { name: 'Название:', value: `${newChannel.name}`, inline: true },
+    { name: 'ID:', value: `<#${newChannel.id}>`, inline: true },
+    { name: 'Пользователь:', value: `<@${auditLog.executor.id}>`, inline: true },
+  ])
+
+  const logChannel = client.channels.cache.get(config.CHANNELS_ID.CHANNELS_LOGS_CHANNEL_ID)
+  if (isTextChannel(logChannel)) {
+    await logChannel.send({ embeds: [embed] })
+  }
 }

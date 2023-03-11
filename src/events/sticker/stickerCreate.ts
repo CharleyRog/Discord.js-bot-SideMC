0da4897465368f3d4e7f533c7e9f60d260952083
CharleyRog <k.ichko.ua@gmail.com>
2023-03-11 18:52:32 +0200

    Error handling added

--- src/events/sticker/stickerCreate.ts
+++ src/events/sticker/stickerCreate.ts
@@ -9,19 +9,23 @@ import config from '../../config/config.json' assert { type: 'json' }
 // CODE
 
 export default async (sticker: any): Promise<void> => {
-  const embed: EmbedBuilder = embedBuilderFoo({
-    title: 'Стикер создан',
-    color: '#00FF00',
-  })
+  try {
+    const embed: EmbedBuilder = embedBuilderFoo({
+      title: 'Стикер создан',
+      color: '#00FF00',
+    })
 
-  embed.addFields([
-    { name: 'Имя:', value: `${sticker.name}`, inline: true },
-    { name: 'ID:', value: `${sticker.id}`, inline: true },
-    { name: 'Пользователь:', value: `<@${sticker.user.id}>`, inline: true },
-  ])
+    embed.addFields([
+      { name: 'Имя:', value: `${sticker.name}`, inline: true },
+      { name: 'ID:', value: `${sticker.id}`, inline: true },
+      { name: 'Пользователь:', value: `<@${sticker.user.id}>`, inline: true },
+    ])
 
-  const logChannel = client.channels.cache.get(config.CHANNELS_ID.STICKER_LOGS_CHANNEL_ID)
-  if (isTextChannel(logChannel)) {
-    await logChannel.send({ embeds: [embed] })
+    const logChannel = client.channels.cache.get(config.CHANNELS_ID.STICKER_LOGS_CHANNEL_ID)
+    if (isTextChannel(logChannel)) {
+      await logChannel.send({ embeds: [embed] })
+    }
+  } catch (error) {
+    console.error(error)
   }
 }
--- src/events/sticker/stickerUpdate.ts
+++ src/events/sticker/stickerUpdate.ts
@@ -9,29 +9,31 @@ import config from '../../config/config.json' assert { type: 'json' }
 // CODE
 
 export default async (oldSticker: any, newSticker: any): Promise<void> => {
-  console.log(`stickerUpdate: ${oldSticker} | ${newSticker}`)
+  try {
+    const embed: EmbedBuilder = embedBuilderFoo({
+      title: 'Стикер изменен',
+      color: '#FF0000',
+    })
 
-  const embed: EmbedBuilder = embedBuilderFoo({
-    title: 'Стикер изменен',
-    color: '#FF0000',
-  })
-
-  embed.addFields([
-    { name: 'ID:', value: `${newSticker.id}`, inline: true },
-    { name: 'Пользователь:', value: `<@${newSticker.user.id}>`, inline: true },
-  ])
-
-  if (oldSticker.name !== newSticker.name) {
     embed.addFields([
-      { name: 'Старое название:', value: `${oldSticker.name}`, inline: true },
-      { name: 'Новое название:', value: `${newSticker.name}`, inline: true },
+      { name: 'ID:', value: `${newSticker.id}`, inline: true },
+      { name: 'Пользователь:', value: `<@${newSticker.user.id}>`, inline: true },
     ])
-  } else {
-    embed.addFields([{ name: 'Название:', value: `${newSticker.name}`, inline: true }])
-  }
 
-  const logChannel = client.channels.cache.get(config.CHANNELS_ID.STICKER_LOGS_CHANNEL_ID)
-  if (isTextChannel(logChannel)) {
-    await logChannel.send({ embeds: [embed] })
+    if (oldSticker.name !== newSticker.name) {
+      embed.addFields([
+        { name: 'Старое название:', value: `${oldSticker.name}`, inline: true },
+        { name: 'Новое название:', value: `${newSticker.name}`, inline: true },
+      ])
+    } else {
+      embed.addFields([{ name: 'Название:', value: `${newSticker.name}`, inline: true }])
+    }
+
+    const logChannel = client.channels.cache.get(config.CHANNELS_ID.STICKER_LOGS_CHANNEL_ID)
+    if (isTextChannel(logChannel)) {
+      await logChannel.send({ embeds: [embed] })
+    }
+  } catch (error) {
+    console.error(error)
   }
 }

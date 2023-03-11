c2abc1543f352e01bf7fd7b8947ca61dd09c9042
CharleyRog <k.ichko.ua@gmail.com>
2023-03-11 19:26:41 +0200

    threadDelete loging added

--- src/events/thread/threadDelete.ts
+++ src/events/thread/threadDelete.ts
@@ -1,7 +1,42 @@
 // IMPORT MODULES
 
+import { EmbedBuilder } from '@discordjs/builders'
+import embedBuilderFoo from '../../utils/embedBuilderFoo.js'
+import { TextChannel } from 'discord.js'
+import client from '../../client.js'
+import isTextChannel from '../../utils/isTextChannel.js'
+import config from '../../config/config.json' assert { type: 'json' }
+
 // CODE
 
 export default async (thread: any): Promise<void> => {
-  console.log(`threadDelete: ${thread}`)
+  try {
+    const embed: EmbedBuilder = embedBuilderFoo({
+      title: 'Стикер создан',
+      color: '#FF0000',
+    })
+
+    if (thread.id) {
+      embed.addFields([{ name: 'Thread ID:', value: thread.id, inline: true }])
+    }
+
+    if (thread.name) {
+      embed.addFields([{ name: 'Thread name:', value: thread.name, inline: true }])
+    }
+
+    if (thread.parent) {
+      embed.addFields([{ name: 'Parent ID:', value: thread.parent.id, inline: true }])
+    }
+
+    if (thread.guild) {
+      embed.addFields([{ name: 'Guild ID:', value: thread.guild.id, inline: true }])
+    }
+
+    const logChannel = client.channels.cache.get(config.CHANNELS_ID.THREADS_LOGS_CHANNEL_ID) as TextChannel
+    if (isTextChannel(logChannel)) {
+      await logChannel.send({ embeds: [embed] })
+    }
+  } catch (error) {
+    console.error(error)
+  }
 }

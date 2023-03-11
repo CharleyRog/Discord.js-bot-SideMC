2920fce2c768f8dec76693a3fb2137b3d4a7da98
CharleyRog <k.ichko.ua@gmail.com>
2023-03-11 18:49:35 +0200

    Error handling added

--- src/events/process/uncaughtException.ts
+++ src/events/process/uncaughtException.ts
@@ -6,19 +6,23 @@ import client from '../../client.js'
 import embedBuilderFoo from '../../utils/embedBuilderFoo.js'
 import isTextChannel from '../../utils/isTextChannel.js'
 
-// uncaughtException EVENT CODE
+// CODE
 
 export default async (err: any): Promise<void> => {
-  console.error('Uncaught Exception: ' + err)
+  try {
+    console.error('Uncaught Exception: ' + err)
 
-  const embed: EmbedBuilder = embedBuilderFoo({
-    title: 'Uncaught Exception',
-    description: `${err}`,
-    color: '#F00000',
-  })
+    const embed: EmbedBuilder = embedBuilderFoo({
+      title: 'Uncaught Exception',
+      description: `${err}`,
+      color: '#F00000',
+    })
 
-  const logChannel = client.channels.cache.get(config.CHANNELS_ID.ERRORS_CHANNEL_ID)
-  if (isTextChannel(logChannel)) {
-    await logChannel.send({ embeds: [embed] })
+    const logChannel = client.channels.cache.get(config.CHANNELS_ID.ERRORS_CHANNEL_ID)
+    if (isTextChannel(logChannel)) {
+      await logChannel.send({ embeds: [embed] })
+    }
+  } catch (error) {
+    console.error(error)
   }
 }
--- src/events/process/unhandledRejection.ts
+++ src/events/process/unhandledRejection.ts
@@ -6,22 +6,26 @@ import client from '../../client.js'
 import embedBuilderFoo from '../../utils/embedBuilderFoo.js'
 import isTextChannel from '../../utils/isTextChannel.js'
 
-// unhandledRejection EVENT CODE
+// CODE
 
 export default async (reason: any, promise: any): Promise<void> => {
-  console.error('[FATAL] Possibly Unhandled Rejection at: Promise ', promise, ' reason: ', reason.message)
+  try {
+    console.error('[FATAL] Possibly Unhandled Rejection at: Promise ', promise, ' reason: ', reason.message)
 
-  const embed: EmbedBuilder = embedBuilderFoo({
-    title: 'Unhandled Promise Rejection',
-    color: '#F00000',
-  })
+    const embed: EmbedBuilder = embedBuilderFoo({
+      title: 'Unhandled Promise Rejection',
+      color: '#F00000',
+    })
 
-  embed.addFields([
-    { name: 'Promise', value: `${promise}` },
-    { name: 'Reason', value: `${reason.message}` },
-  ])
-  const logChannel = client.channels.cache.get(config.CHANNELS_ID.ERRORS_CHANNEL_ID)
-  if (isTextChannel(logChannel)) {
-    await logChannel.send({ embeds: [embed] })
+    embed.addFields([
+      { name: 'Promise', value: `${promise}` },
+      { name: 'Reason', value: `${reason.message}` },
+    ])
+    const logChannel = client.channels.cache.get(config.CHANNELS_ID.ERRORS_CHANNEL_ID)
+    if (isTextChannel(logChannel)) {
+      await logChannel.send({ embeds: [embed] })
+    }
+  } catch (error) {
+    console.error(error)
   }
 }

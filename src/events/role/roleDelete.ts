845f31667f90ac1080744cdb121968b4c4b41343
CharleyRog <k.ichko.ua@gmail.com>
2023-03-11 18:51:33 +0200

    Error handling added

--- src/events/role/roleDelete.ts
+++ src/events/role/roleDelete.ts
@@ -10,23 +10,27 @@ import typesOfAuditLogs from '../../static/typesOfAuditLogs.js'
 // CODE
 
 export default async (role: any): Promise<void> => {
-  const auditLog = await role.guild
-    .fetchAuditLogs({ type: typesOfAuditLogs.ROLE_DELETE })
-    .then((audit: { entries: { first: () => any } }) => audit.entries.first())
+  try {
+    const auditLog = await role.guild
+      .fetchAuditLogs({ type: typesOfAuditLogs.ROLE_DELETE })
+      .then((audit: { entries: { first: () => any } }) => audit.entries.first())
 
-  const embed: EmbedBuilder = embedBuilderFoo({
-    title: 'Роль удалена',
-    color: '#FF0000',
-  })
+    const embed: EmbedBuilder = embedBuilderFoo({
+      title: 'Роль удалена',
+      color: '#FF0000',
+    })
 
-  embed.addFields([
-    { name: 'Название:', value: `${role.name}`, inline: true },
-    { name: 'ID:', value: `${role.id}`, inline: true },
-    { name: 'Пользователь:', value: `<@${auditLog.executor.id}>`, inline: true },
-  ])
+    embed.addFields([
+      { name: 'Название:', value: `${role.name}`, inline: true },
+      { name: 'ID:', value: `${role.id}`, inline: true },
+      { name: 'Пользователь:', value: `<@${auditLog.executor.id}>`, inline: true },
+    ])
 
-  const logChannel = client.channels.cache.get(config.CHANNELS_ID.ROLE_LOGS_CHANNEL_ID)
-  if (isTextChannel(logChannel)) {
-    await logChannel.send({ embeds: [embed] })
+    const logChannel = client.channels.cache.get(config.CHANNELS_ID.ROLE_LOGS_CHANNEL_ID)
+    if (isTextChannel(logChannel)) {
+      await logChannel.send({ embeds: [embed] })
+    }
+  } catch (error) {
+    console.log(error)
   }
 }
--- src/events/role/roleUpdate.ts
+++ src/events/role/roleUpdate.ts
@@ -10,65 +10,72 @@ import typesOfAuditLogs from '../../static/typesOfAuditLogs.js'
 // CODE
 
 export default async (oldRole: any, newRole: any): Promise<void> => {
-  const auditLog = await newRole.guild
-    .fetchAuditLogs({ type: typesOfAuditLogs.ROLE_UPDATE })
-    .then((audit: { entries: { first: () => any } }) => audit.entries.first())
+  try {
+    const auditLog = await newRole.guild
+      .fetchAuditLogs({ type: typesOfAuditLogs.ROLE_UPDATE })
+      .then((audit: { entries: { first: () => any } }) => audit.entries.first())
 
-  const embed: EmbedBuilder = embedBuilderFoo({
-    title: 'Роль обновлена',
-    color: '#FFFF00',
-  })
+    const embed: EmbedBuilder = embedBuilderFoo({
+      title: 'Роль обновлена',
+      color: '#FFFF00',
+    })
 
-  if (oldRole.color !== newRole.color) {
-    embed
-      .setDescription('Цвет изменен')
-      .addFields([
+    if (oldRole.color !== newRole.color) {
+      embed
+        .setDescription('Цвет изменен')
+        .addFields([
+          { name: 'ID:', value: `${newRole.id}`, inline: true },
+          { name: 'Пользователь:', value: `<@${auditLog.executor.id}>`, inline: true },
+          { name: 'Название:', value: newRole.name, inline: true },
+        ])
+        .addFields([
+          { name: 'Старый цвет:', value: `${oldRole.hexColor}`, inline: true },
+          { name: 'Новый цвет:', value: `${newRole.hexColor}`, inline: true },
+        ])
+    }
+
+    if (oldRole.name !== newRole.name) {
+      embed
+        .setDescription('Название изменено')
+        .addFields([
+          { name: 'ID:', value: `${newRole.id}`, inline: true },
+          { name: 'Пользователь:', value: `<@${auditLog.executor.id}>`, inline: true },
+        ])
+        .addFields([
+          { name: 'Старое название:', value: `${oldRole.name}`, inline: true },
+          { name: 'Новое название:', value: `${newRole.name}`, inline: true },
+        ])
+    }
+
+    if (oldRole.permissions.bitfield !== newRole.permissions.bitfield) {
+      const addedPermissions = newRole.permissions.missing(oldRole.permissions)
+      const removedPermissions = oldRole.permissions.missing(newRole.permissions)
+
+      embed.addFields([
         { name: 'ID:', value: `${newRole.id}`, inline: true },
         { name: 'Пользователь:', value: `<@${auditLog.executor.id}>`, inline: true },
         { name: 'Название:', value: newRole.name, inline: true },
       ])
-      .addFields([
-        { name: 'Старый цвет:', value: `${oldRole.hexColor}`, inline: true },
-        { name: 'Новый цвет:', value: `${newRole.hexColor}`, inline: true },
-      ])
-  }
 
-  if (oldRole.name !== newRole.name) {
-    embed
-      .setDescription('Название изменено')
-      .addFields([
-        { name: 'ID:', value: `${newRole.id}`, inline: true },
-        { name: 'Пользователь:', value: `<@${auditLog.executor.id}>`, inline: true },
-      ])
-      .addFields([
-        { name: 'Старое название:', value: `${oldRole.name}`, inline: true },
-        { name: 'Новое название:', value: `${newRole.name}`, inline: true },
-      ])
-  }
+      if (addedPermissions.length) {
+        embed.setDescription('Разрешения удалены').addFields({
+          name: 'Разрешения:',
+          value: addedPermissions.join(', '),
+        })
+      }
 
-  if (oldRole.permissions.bitfield !== newRole.permissions.bitfield) {
-    const addedPermissions = newRole.permissions.missing(oldRole.permissions)
-    const removedPermissions = oldRole.permissions.missing(newRole.permissions)
-
-    embed.addFields([
-      { name: 'ID:', value: `${newRole.id}`, inline: true },
-      { name: 'Пользователь:', value: `<@${auditLog.executor.id}>`, inline: true },
-      { name: 'Название:', value: newRole.name, inline: true },
-    ])
-
-    if (addedPermissions.length) {
-      embed.setDescription('Разрешения удалены').addFields({ name: 'Разрешения:', value: addedPermissions.join(', ') })
+      if (removedPermissions.length) {
+        embed
+          .setDescription('Разрешения добавлены')
+          .addFields({ name: 'Разрешения:', value: removedPermissions.join(', ') })
+      }
     }
 
-    if (removedPermissions.length) {
-      embed
-        .setDescription('Разрешения добавлены')
-        .addFields({ name: 'Разрешения:', value: removedPermissions.join(', ') })
+    const logChannel = client.channels.cache.get(config.CHANNELS_ID.ROLE_LOGS_CHANNEL_ID)
+    if (isTextChannel(logChannel)) {
+      await logChannel.send({ embeds: [embed] })
     }
-  }
-
-  const logChannel = client.channels.cache.get(config.CHANNELS_ID.ROLE_LOGS_CHANNEL_ID)
-  if (isTextChannel(logChannel)) {
-    await logChannel.send({ embeds: [embed] })
+  } catch (error) {
+    console.log(error)
   }
 }

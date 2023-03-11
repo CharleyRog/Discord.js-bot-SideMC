a797a2975942adb6a653140e41ff8736302e0f18
CharleyRog <k.ichko.ua@gmail.com>
2023-03-11 18:40:05 +0200

    channelUpdate logs fix

--- src/events/channel/channelUpdate.ts
+++ src/events/channel/channelUpdate.ts
@@ -19,18 +19,94 @@ export default async (oldChannel: any, newChannel: any): Promise<void> => {
     color: '#FFFF00',
   })
 
-  if (newChannel.name !== oldChannel.name) {
-    embed.addFields([
-      { name: 'Старое название:', value: oldChannel.name, inline: true },
-      { name: 'Новое название:', value: newChannel.name, inline: true },
-    ])
-  }
-
   embed.addFields([
-    { name: 'Название:', value: `${newChannel.name}`, inline: true },
+    { name: 'Название:', value: `${oldChannel.name}`, inline: true },
     { name: 'ID:', value: `<#${newChannel.id}>`, inline: true },
     { name: 'Пользователь:', value: `<@${auditLog.executor.id}>`, inline: true },
   ])
+
+  if (oldChannel.name !== newChannel.name) {
+    embed.addFields([
+      { name: 'oldChannel.name:', value: oldChannel.name, inline: true },
+      { name: 'newChannel.name:', value: newChannel.name, inline: true },
+    ])
+  }
+
+  if (oldChannel.type !== newChannel.type) {
+    embed.addFields([
+      { name: 'oldChannel.type:', value: oldChannel.type, inline: true },
+      { name: 'newChannel.type:', value: newChannel.type, inline: true },
+    ])
+  }
+
+  if (oldChannel.parentId !== newChannel.parentId) {
+    embed.addFields([
+      { name: 'oldChannel.parentId:', value: oldChannel.parentId, inline: true },
+      { name: 'newChannel.parentId:', value: newChannel.parentId, inline: true },
+    ])
+  }
+
+  if (oldChannel.rawPosition !== newChannel.rawPosition) {
+    embed.addFields([
+      { name: 'oldChannel.rawPosition:', value: oldChannel.rawPosition, inline: true },
+      { name: 'newChannel.rawPosition:', value: newChannel.rawPosition, inline: true },
+    ])
+  }
+
+  if (oldChannel?.nsfw !== newChannel?.nsfw) {
+    embed.addFields([
+      { name: 'oldChannel.nsfw:', value: oldChannel.nsfw, inline: true },
+      { name: 'newChannel.nsfw:', value: newChannel.nsfw, inline: true },
+    ])
+  }
+
+  if (oldChannel.topic !== newChannel.topic) {
+    embed.addFields([
+      { name: 'oldChannel.topic:', value: oldChannel.topic, inline: true },
+      { name: 'newChannel.topic:', value: newChannel.topic, inline: true },
+    ])
+  }
+
+  if (oldChannel.permissionOverwrites !== newChannel.permissionOverwrites) {
+    console.log(
+      `Permission overwrites changed from ${JSON.stringify(oldChannel.permissionOverwrites)} to ${JSON.stringify(
+        newChannel.permissionOverwrites,
+      )}`,
+    )
+    embed.addFields([
+      {
+        name: 'Permission overwrites from:',
+        value: JSON.stringify(oldChannel.permissionOverwrites),
+        inline: true,
+      },
+      {
+        name: 'Permission overwrites to:',
+        value: JSON.stringify(newChannel.permissionOverwrites),
+        inline: true,
+      },
+    ])
+  }
+
+  if (oldChannel.bitrate !== newChannel.bitrate) {
+    embed.addFields([
+      { name: 'oldChannel.bitrate:', value: oldChannel.bitrate, inline: true },
+      { name: 'newChannel.bitrate:', value: newChannel.bitrate, inline: true },
+    ])
+  }
+
+  if (oldChannel.userLimit !== newChannel.userLimit) {
+    embed.addFields([
+      { name: 'oldChannel.userLimit:', value: oldChannel.userLimit, inline: true },
+      { name: 'newChannel.userLimit:', value: newChannel.userLimit, inline: true },
+    ])
+  }
+
+  if (oldChannel.videoQualityMode !== newChannel.videoQualityMode) {
+    embed.addFields([
+      { name: 'oldChannel.videoQualityMode:', value: oldChannel.videoQualityMode, inline: true },
+      { name: 'newChannel.videoQualityMode:', value: newChannel.videoQualityMode, inline: true },
+    ])
+  }
 
   const logChannel = client.channels.cache.get(config.CHANNELS_ID.CHANNELS_LOGS_CHANNEL_ID)
   if (isTextChannel(logChannel)) {

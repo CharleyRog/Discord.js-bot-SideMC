4801bb6fe01271f9285ef79c13c9c5d52716af59
CharleyRog <k.ichko.ua@gmail.com>
2023-03-12 01:01:31 +0200

    Minor fix

--- src/utils/registerCommands.ts
+++ src/utils/registerCommands.ts
@@ -10,14 +10,9 @@ import { TOKEN, CLIENT_ID } from './devModeChecker.js'
 
 const registerCommands = async (): Promise<void> => {
   const rest = new REST({ version: '10' }).setToken(TOKEN)
-
-  try {
-    console.log('Started refreshing application (/) commands.')
-    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, config.GUILD_ID), { body: commands })
-    console.log('Successfully reloaded application (/) commands.')
-  } catch (error) {
-    console.error(error)
-  }
+  console.log('Started refreshing application (/) commands.')
+  await rest.put(Routes.applicationGuildCommands(CLIENT_ID, config.GUILD_ID), { body: commands })
+  console.log('Successfully reloaded application (/) commands.')
 }
 
 export default registerCommands

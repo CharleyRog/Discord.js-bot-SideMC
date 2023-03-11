c482cde3d7b9f2388b754576f18231595d9499fe
CharleyRog <k.ichko.ua@gmail.com>
2023-03-11 17:58:59 +0200

    Error logs added

--- src/events/client/error.ts
+++ src/events/client/error.ts
@@ -1,7 +1,42 @@
 // IMPORT MODULES
 
+import typesOfAuditLogs from '../../static/typesOfAuditLogs.js'
+import { EmbedBuilder } from '@discordjs/builders'
+import embedBuilderFoo from '../../utils/embedBuilderFoo.js'
+import client from '../../client.js'
+import isTextChannel from '../../utils/isTextChannel.js'
+import config from '../../config/config.json' assert { type: 'json' }
+
 // CODE
 
 export default async (error: any): Promise<void> => {
-  console.log(`error: ${error}`)
+  const embed: EmbedBuilder = embedBuilderFoo({
+    title: 'Отловлена ошибка',
+    color: '#FF0000',
+  })
+
+  if (error.message) {
+    embed.addFields([{ name: 'Message:', value: error.message, inline: true }])
+  }
+
+  if (error.method) {
+    embed.addFields([{ name: 'Method:', value: error.method, inline: true }])
+  }
+
+  if (error.path) {
+    embed.addFields([{ name: 'Path:', value: error.path, inline: true }])
+  }
+
+  if (error.code) {
+    embed.addFields([{ name: 'Code:', value: error.code, inline: true }])
+  }
+
+  if (error.httpStatus) {
+    embed.addFields([{ name: 'HttpStatus:', value: error.httpStatus, inline: true }])
+  }
+
+  const logChannel = client.channels.cache.get(config.CHANNELS_ID.ERRORS_CHANNEL_ID)
+  if (isTextChannel(logChannel)) {
+    await logChannel.send({ embeds: [embed] })
+  }
 }
